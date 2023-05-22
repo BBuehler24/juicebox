05870438842ff675c66e5212381a7d2d2d48630c
BBuehler24 <bbuehler47@yahoo.com>
2023-05-21 21:14:29 -0500

    updates

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const tagsRouter = express.Router();
 const { getAllTags } = require('../db');
+const { getPostsByTagName } = require('../db');
+
 
 tagsRouter.get('/', async (req, res) => {
     const tags = await getAllTags();
@@ -9,5 +11,30 @@ tagsRouter.get('/', async (req, res) => {
         tags
     });
 });
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    const { tagName } = req.params;
+  
+    try {
+      const allPosts = await getPostsByTagName(tagName);
+      const posts = allPosts.filter(
+        (post) =>
+          (post.active && post.author.active) || post.author.id === req.user?.id
+      );
+  
+      if (posts.length > 0) {
+        res.send({
+          posts
+        });
+      } else {
+        next({
+          name: 'TagNameDoesNotExist',
+          message: 'This Tag name does not exist, try another.'
+        });
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
 
 module.exports = tagsRouter;
--- db/index.js
+++ db/index.js
@@ -84,9 +84,19 @@ async function getUserById(userId) {
   }
 }
 
-/**
- * POST Methods
- */
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `, [username]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function createPost({
   authorId,
@@ -182,6 +192,13 @@ async function getPostById(postId) {
       FROM posts
       WHERE id=$1;
     `, [postId]);
+
+    if (!post) {
+      throw {
+        name: "PostNotFoundError",
+        message: "Could not find a post with that postId"
+      };
+    }
 
     const { rows: tags } = await client.query(`
       SELECT tags.*
@@ -327,6 +344,7 @@ module.exports = {
   updateUser,
   getAllUsers,
   getUserById,
+  getUserByUsername,
   createPost,
   updatePost,
   getAllPosts,
